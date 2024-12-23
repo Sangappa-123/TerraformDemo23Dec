2917b563804a847e147bea0464b80ecb866db4bf
Sangappa-123 <birajsd@gmail.com>
2024-12-23 12:50:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,21 @@ resource "aws_s3_bucket" "example" {
   acl    = "private"
 }
 
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
+}
+
 variable "bucket_name" {
   type = string
+}
+
+variable "ami" {
+  description = "AMI ID of the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
 }
